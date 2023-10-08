8fc36e55d7960531481e20f6f204891aac8b3487
hizbullahjan <hizbullahjan996@gmail.com>
2023-10-08 05:49:58 -0700

    es6

---
+++ script.mjs
@@ -0,0 +1,7 @@
+console.log ("Hello word from mjs");
+let myNumber = 6;
+let myName = "Hizb Ullah Jan";
+console.log (myNumber, myName)
+myNumber = 20;
+myName = "Hizb Ullah Jani"
+console.log (myName, myNumber)
